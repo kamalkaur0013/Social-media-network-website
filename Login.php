2b20a5051feaf30f6b92f0c961fd5c56f29e9fa3
haads23 <32466548+haads23@users.noreply.github.com>
2018-10-31 18:42:10 -0400

    Login.php 1. added header and footer

--- Login.php
+++ Login.php
@@ -5,8 +5,14 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+    <?php
+        include './ProjectCommon/Header.php';
+    ?>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <title></title>
     </head>
     <body>
@@ -17,4 +23,7 @@ and open the template in the editor.
         //test 
         ?>
     </body>
+    <?php
+        include './ProjectCommon/Footer.php';
+    ?>
 </html>
