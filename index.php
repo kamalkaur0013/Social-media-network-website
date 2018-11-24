c5c2293b6f0073ba2114952e80cecfa7e9d4ccb0
haads23 <32466548+haads23@users.noreply.github.com>
2018-11-24 15:58:01 -0500

    Index Page

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-
+<?php
+include("./ProjectCommon/Header.php");
+?>
 <html>
-    <?php
-        include './ProjectCommon/Header.php';
-    ?>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        // Test 123 
-        ?>
-    </body>
-     <?php
-        include './ProjectCommon/Footer.php';
-    ?>
+<head>
+    <title>Algonquin Social Media</title>
+    
+</head>
+<body>
+    <div class="container">
+        </br>
+    <h1>Welcome to Algonquin Social Media Website</h1>
+    <p>If you have never used this before, you have to <a href="./NewUser.php">sign up</a> first.</p>
+    <p>If you have already signed up, you can <a href ="./Login.php">login</a> now.</p>
+    </div>
+<?php include("./ProjectCommon/Footer.php"); ?>
+</body>
+
 </html>
