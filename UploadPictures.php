eab5108d457d4c5f83c69b229ca5cbf596a1e121
anda0012 <31903950+anda0012@users.noreply.github.com>
2018-11-24 15:31:57 -0500

    upload pics testing

--- UploadPictures.php
+++ UploadPictures.php
@@ -19,6 +19,42 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
+        
+            <div class="container">
+            <h1>Upload Pictures</h1>
+
+            <p>Accepted picture types: JPG(JPEG), GIF and PNG.</p>
+
+            <p>You can upload multiple pictures at a time by pressing the shift key while selecting pictures.</p>
+            
+            <p>When uploading multiple pictures, the title and description fields will be applied to all pictures.</p>
+            
+            <!--<span class='error' style="color:red"><?php echo $error;?></span>-->
+            
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+                
+                <div class="row">   
+                    <div class="form-group">
+                        <!--<div class="col-xs-4">-->
+                            <label for="fileUpload">File to Upload:</label>
+                            <input type="file" class="form-control" id="fileUpload" name="imageUpload[]" multiple>                            
+                        <!--</div>-->
+                    </div>
+                 </div>
+                
+                <br /><br/>
+
+                <div class="row">
+                    <div class="form-group">
+                        <!--<div class="col-xs-4">-->
+                            <button type="submit" class="btn btn-primary" value="Submit" name="uploadBtn">Upload</button>
+                            &nbsp; &nbsp; &nbsp;<button type="reset" class="btn btn-primary">Reset</button>
+                        <!--</div>-->
+                    </div>
+                </div>  
+                
+            </form>
+        </div>
     </body>
     <?php
         include './ProjectCommon/Footer.php';
